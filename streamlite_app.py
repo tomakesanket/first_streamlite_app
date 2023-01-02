d13da5d5214ec1103950aaa07d7da0159407f944
tomakesanket <121787475+tomakesanket@users.noreply.github.com>
2023-01-02 14:16:34 +0530

    Update streamlite_app.py
    Added breakfast menu header and 3 items

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,7 +1,6 @@
 import streamlit
-streamlit.title("Happy New Year")
-
-streamlit.header("Welcome 2023")
-streamlit.text('We are entering int 2023')
-streamlit.text('Wishing you good luck and Happiness')
-streamlit.text('Hope you will enjoy this year')
+streamlit.title("My Parents Healthy Dinner")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
