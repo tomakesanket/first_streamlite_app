01351c53415fdd091272f202efc23cad91dc3de8
tomakesanket <121787475+tomakesanket@users.noreply.github.com>
2023-01-02 14:13:08 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title("Happy New Year")
+
+streamlit.header("Welcome 2023")
+streamlit.text('We are entering int 2023')
+streamlit.text('Wishing you good luck and Happiness')
+streamlit.text('Hope you will enjoy this year')
