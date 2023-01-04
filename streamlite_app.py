7c333179323174451d63aaceea3e670d00bf910d
tomakesanket <121787475+tomakesanket@users.noreply.github.com>
2023-01-04 18:09:31 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -47,3 +47,10 @@ streamlit.text(my_data_row)
 
 add_my_fruit = streamlit.text_input("Second Text entry box","jackfruit")
 streamlit.write('The user entered ', add_my_fruit)
+streamlit.write("Thanks adding my fruit", add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+
+
+
+
